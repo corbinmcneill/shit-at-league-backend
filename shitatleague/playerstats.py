3dc4df7c1d3f8de1f3f35a03ed3c27b61983ab01
Corbin McNeill <corbin.mc96@gmail.com>
2021-03-03 00:44:11 -0700

    added some stuff to the statistic engine

--- shitatleague/playerstats.py
+++ shitatleague/playerstats.py
@@ -8,21 +8,25 @@ class PlayerStats:
         self.region = region
         self.summonerId = summoner_id
         self.riotClient = riot_client
+        self.callback = callback
+
+        self.matches = None
+        self.matchIds = None
         self.wasSuccessful = False
         self.finishedComputing = False
 
+    def generate(self):
+        # TODO missing stuff here
         try:
-            self.matchIds = self.get_match_ids();
-            self.matches = {}
             for matchId in self.matchIds:
                 self.matches[matchId] = self.get_match_by_id(matchId)
             self.wasSuccessful = True
-        except Exception:
+        except RiotClient.APIException:
             print("Exception occurred")
             # TODO
         finally:
             self.finishedComputing = True
-            callback()
+            self.callback()
 
     def is_resolved(self) -> bool:
         return self.finishedComputing
---
+++ shitatleague/statistic.py
@@ -0,0 +1,5 @@
+class Statistic:
+    def __init__(self, score, first_line, second_line=None):
+        self.score = score
+        self.firstLine = first_line
+        self.secondLine = second_line
